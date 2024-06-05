aed0281f6eaa7ed90ed879698e23cfe8757686a2
Adam Richardson <adam.richardson@cgi.com>
2024-06-05 12:23:53 +0100

    Variablise curl command and improve output

--- backup_my_confluence_content.bash
+++ backup_my_confluence_content.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 die() {
 	echo $1
@@ -10,7 +10,9 @@ die() {
 
 . ./conf
 
-[ ! -e content.html ] && curl -u ${CRED} https://cannonst.com/confluence/display/~adam.richardson/My+Content ${CERTFLAGS} 2>/dev/null >content.html
+CURLCMD="curl --no-progress-meter -u ${CRED} ${CERTFLAGS}"
+
+[ ! -e content.html ] && ${CURLCMD} https://cannonst.com/confluence/display/~adam.richardson/My+Content 2>/dev/null >content.html
 
 # Gliffies export as image. That'll do.
 PAGES=$(grep "Page:</span> *<a href" content.html | cut -c 143- | cut -d\" -f1)
@@ -25,28 +27,32 @@ PAGEDIR="pages"
 install -d ./${ASSETDIR} -m0755
 for PNG in ${PNGS}
 do
+	echo "Getting ${PNG}..."
 	PNGF=${ASSETDIR}/$(<<<"${PNG}" tr '/' '_')
-	[ ! -e "${PNGF}" ] && curl -u ${CRED} https://cannonst.com${PNG} ${CERTFLAGS} -o "${PNGF}"
+	[ ! -e "${PNGF}" ] && ${CURLCMD} https://cannonst.com${PNG} -o "${PNGF}"
 done
 
 for JPG in ${JPGS}
 do
+	echo "Getting ${JPG}..."
 	JPGF=${ASSETDIR}/$(<<<"${JPG}" tr '/' '_')
-	[ ! -e "${JPGF}" ] && curl -u ${CRED} https://cannonst.com${JPG} ${CERTFLAGS} -o "${JPGF}"
+	[ ! -e "${JPGF}" ] && ${CURLCMD} https://cannonst.com${JPG} -o "${JPGF}"
 done
 
 for PDF in ${PDFS}
 do
+	echo "Getting ${PDF}..."
 	PDFF=${ASSETDIR}/$(<<<"${PDF}" tr '/' '_')
-	[ ! -e "${PDFF}" ] && curl -u ${CRED} https://cannonst.com${PDF} ${CERTFLAGS} -o "${PDFF}"
+	[ ! -e "${PDFF}" ] && ${CURLCMD} https://cannonst.com${PDF} -o "${PDFF}"
 done
 
 # BUG: URLs don't always match file paths, there is a missing %25 ('%') wherever there is an entity. The URL isn't being HTML-encoded. Confluence bug? Workaround: add missing entity?
 install -d ./${PAGEDIR} -m0755
 for PAGE in ${PAGES}
 do
+	echo "Getting ${PAGE}..."
 	PAGEF=${PAGEDIR}/$(<<<"${PAGE}" tr '/' '_').html
-	[ ! -e "${PAGEF}" ] && curl -u ${CRED} https://cannonst.com${PAGE} ${CERTFLAGS} -o "${PAGEF}"
+	[ ! -e "${PAGEF}" ] && ${CURLCMD} https://cannonst.com${PAGE} -o "${PAGEF}"
 	# relink css
 	sed -i -e '/link rel=.stylesheet.*css/s/\//_/g' "${PAGEF}"
 	# relink images
@@ -55,12 +61,15 @@ done
 
 for CSS in ${CSSS}
 do
+	echo "Getting ${CSS}..."
 	CSSF=${PAGEDIR}/$(<<<"${CSS}" tr '/' '_')
-	[ ! -e "${CSSF}" ] && curl -u ${CRED} https://cannonst.com${CSS} ${CERTFLAGS} -o "${CSSF}"
+	[ ! -e "${CSSF}" ] && ${CURLCMD} https://cannonst.com${CSS} -o "${CSSF}"
 done
 
-
 TARDIRS="${ASSETDIR} ${PAGEDIR}"
+echo "Making tarball..."
 TARBALL="$(date +%Y%m%d)_confluence_backup.tgz"
 [ ! -e "${TARBALL}" ] && tar cpzf "${TARBALL}" ${TARDIRS}
 # rm -rf ${TARDIRS}
+
+echo "Done."
