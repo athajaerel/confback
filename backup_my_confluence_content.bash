564d0cf0ee2c90b3a30649d15a168540f43737e6
Adam Richardson <adam.richardson@cgi.com>
2024-06-05 16:02:36 +0100

    Export PDF versions of pages

--- backup_my_confluence_content.bash
+++ backup_my_confluence_content.bash
@@ -11,6 +11,7 @@ die() {
 . ./conf
 
 CURLCMD="curl --no-progress-meter -u ${CRED} ${CERTFLAGS}"
+HEADERFILE="/dev/shm/headers_out"
 
 [ ! -e content.html ] && ${CURLCMD} https://cannonst.com/confluence/display/~adam.richardson/My+Content 2>/dev/null >content.html
 
@@ -23,6 +24,7 @@ CSSS=$(grep "stylesheet" content.html | cut -c 30- | cut -d\? -f1 | cut -d\" -f1
 
 ASSETDIR="assets"
 PAGEDIR="pages"
+PDFDIR="pdfs"
 
 install -d ./${ASSETDIR} -m0755
 for PNG in ${PNGS}
@@ -48,6 +50,7 @@ done
 
 # BUG: URLs don't always match file paths, there is a missing %25 ('%') wherever there is an entity. The URL isn't being HTML-encoded. Confluence bug? Workaround: add missing entity?
 install -d ./${PAGEDIR} -m0755
+install -d ./${PDFDIR} -m0755
 for PAGE in ${PAGES}
 do
 	echo "Getting ${PAGE}..."
@@ -57,6 +60,50 @@ do
 	sed -i -e '/link rel=.stylesheet.*css/s/\//_/g' "${PAGEF}"
 	# relink images
 	sed -i -e 's:/:_:g; s:<_:</:g; s:_>:/>:g; s:src=":src="../assets/:g' "${PAGEF}"
+
+	echo "Getting ${PAGE} PDF..."
+
+	# get pageID
+	PAGEID=""
+	ACT=$(<<<"${PAGE}" cut -d/ -f3)
+	if [ "x${ACT}" == "xpages" ] ; then
+		# looks like: /confluence/pages/viewpage.action?pageId=1234
+		PAGEID=$(<<<${PAGE} cut -d= -f2)
+	elif [ "x${ACT}" == "xdisplay" ] ; then
+		# looks like: /confluence/display/spaceKey/Title
+		SPACEKEY=$(<<<${PAGE} cut -d/ -f4)
+		TITLE=$(<<<${PAGE} cut -d/ -f5-)
+		JSONURL="https://cannonst.com/confluence/rest/api/content?title=${TITLE}&spaceKey=${SPACEKEY}&expand=history"
+		JSON=$(${CURLCMD} "${JSONURL}")
+		SIZE=$(<<<${JSON} jq .size)
+		if [ "x${SIZE}" != "x1" ] ; then
+			echo "Ambiguous result, couldn't get PDF."
+			continue
+		fi
+		PAGEID=$(<<<$JSON jq .results[0].id | cut -d\" -f2)
+	fi
+	if [ "x${PAGEID}" == "x" ] ; then
+		echo "Couldn't get page ID --> no PDF."
+		continue
+	fi
+	echo "Page ID: ${PAGEID}"
+
+	# get pdf version of page
+	# sed because I stupidly have page titles with "..."
+	# which obviously filesystems do not like
+	PDFF=${PDFDIR}/$(<<<"${PAGE}" tr '/' '_' | sed -e 's/\.\.\./_/g').pdf
+	PDFURL="https://cannonst.com/confluence/spaces/flyingpdf/pdfpageexport.action?pageId=${PAGEID}"
+	if [ ! -e "${PDFF}" ] ; then
+		${CURLCMD} "${PDFURL}" -D ${HEADERFILE}
+		grep -q "HTTP/1.1 302" ${HEADERFILE}
+		IS_302=$?
+		if [ $IS_302 -ne 0 ] ; then
+			echo "No redirect --> no PDF."
+			continue
+		fi
+		LOC="https://cannonst.com$(grep "^Location:" ${HEADERFILE} | cut -c 11- | tr -d '\r\n\f')"
+		${CURLCMD} "${LOC}" -o "${PDFF}"
+	fi
 done
 
 for CSS in ${CSSS}
@@ -66,8 +113,8 @@ do
 	[ ! -e "${CSSF}" ] && ${CURLCMD} https://cannonst.com${CSS} -o "${CSSF}"
 done
 
-TARDIRS="${ASSETDIR} ${PAGEDIR}"
 echo "Making tarball..."
+TARDIRS="${ASSETDIR} ${PAGEDIR} ${PDFDIR}"
 TARBALL="$(date +%Y%m%d)_confluence_backup.tgz"
 [ ! -e "${TARBALL}" ] && tar cpzf "${TARBALL}" ${TARDIRS}
 # rm -rf ${TARDIRS}
